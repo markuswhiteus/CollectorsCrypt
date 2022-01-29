11ae05e847ca35bf7031155a974780d2b17c0bbc
Coswold <mark1_321@hotmail.com>
2022-01-29 15:22:34 +0000

    Remove unused themes

--- themes/Jevelin/dist/js/nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".burgermenu").click(function(){
-      $(".mobilemenu").animate({
-        height: 'toggle'
-      });
-    });
-  });
--- themes/Jevelin/dist/js/production-dist.js
+++
@@ -1,9 +0,0 @@
-// binds $ to jquery, requires you to write strict code. Will fail validation if it doesn't match requirements.
-(function($) {
-    "use strict";
-
-	// add all of your code within here, not above or below
-	$(function() {
-    });
-
-});
