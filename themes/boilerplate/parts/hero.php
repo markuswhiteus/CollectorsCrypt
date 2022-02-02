58eaaeffa3335a3ad3984e30bb1020e337754693
Coswold <mark1_321@hotmail.com>
2022-02-02 14:23:14 +0000

    Enabled Dynamic Links In Header

--- themes/boilerplate/parts/hero.php
+++ themes/boilerplate/parts/hero.php
@@ -4,9 +4,9 @@
             <div class="ml-auto pr-10 items-center">
                 <ul class="flex flex-row gap-8 items-center">
                     <li><i class="fas fa-search fa-xl"></i></li>
-                    <li><a href="https://wordpress.coswold.co.uk/?page_id=82"><i class="fas fa-user fa-xl"></a></i></li>
+                    <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><i class="fas fa-user fa-xl"></a></i></li>
                     <li class="flex items-center gap-0.5">
-                        <a href="https://wordpress.coswold.co.uk/?page_id=80">
+                        <a href="<?php echo wc_get_cart_url() ?>">
                             <i class="fas fa-shopping-cart fa-xl cart-icon"></i>
                             <span class="text-white text-xs bg-black rounded-full p-0.5 px-1">
                             <?php echo WC()->cart->get_cart_contents_count(); ?>
@@ -29,7 +29,7 @@
                         ?>
                     </div>
                     <div class="mx-4">
-                        <a href="https://wordpress.coswold.co.uk/">
+                        <a href="<?php echo get_home_url() ?>">
                             <?php 
                                 $image = get_field('site-logo', 'option');
                                 if( !empty( $image ) ): ?>
